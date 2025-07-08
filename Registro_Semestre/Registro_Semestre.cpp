2ee2a255b4f07d24e427a287836b685dcf5e12f9
Israel Burgos <israelburgos2006@gmail.com>
2025-07-08 00:08:31 -0500

    Se agrego la estructura principal para comenzar a hacer el deber

--- Registro_Semestre/Registro_Semestre.cpp
+++ Registro_Semestre/Registro_Semestre.cpp
@@ -1,19 +1,17 @@
-// Registro_Semestre.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
-
 #include <iostream>
+struct Registro {
+    string nombre_estudiante;//el nombre del estudiante será 
+    ///tomado de manera 
+    //aleatoria de una lista de 10 nombres
+    string asignatura;
+    float nota[4];//La nota tiene 4 componentes, el primero sobre 20 
+    //puntos, el segundo sobre 20, el tercero y cuarto    //sobre 30 cada uno. 
+    bool activo; // Indica si el registro está activo (true) 
+    // o fue eliminado (false)
+};
+
 
 int main()
 {
     std::cout << "Hello World!\n";
 }
-
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
-
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
